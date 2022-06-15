462a10e2dc1bd5823051e388506acf740fc63d43
Tuure <tuure.vairio@gmail.com>
2022-06-15 22:26:52 +0300

    No energy right now to make a settings menu, but added timed measurement

--- src/main.cpp
+++ src/main.cpp
@@ -13,23 +13,11 @@
 #define echoPin 4 // to pin Echo of HC-SR04
 #define trigPin 0 // to pin Trig of HC-SR04
 #define led 2
+#define voltRead 
 
 //Telegram Stuff
-
-
-// struct Config {
-//   String bot_token;
-//   String main_chat_id;
-//   String ssid;
-//   String password;
-//   int warn_level;
-//   int top_distance;
-//   int diameter;
-//   int status_every;
-//   String status_period;
-// };
-
-
+bool settings_menu = false;
+bool auto_menu = false;
 
 //SD card stuff
 const char *conf_file = "CessPoolConf.txt";
@@ -42,15 +30,22 @@ JsonObject configJson;
 File configFileSDcard;
 
 
-const unsigned long BOT_MTBS = 1000;
+const unsigned long BOT_MTBS = 800;
 unsigned long bot_lasttime;
 
 
 long duration; // variable for the duration of sound wave travel
 int distance; // variable for the distance measurement
 int warning1 = 0;
-
+int last_time_measured = 0;
 int ledStatus = 0;
+
+int warn_level = 0;
+int top_distance = 0;
+int diameter = 0;
+int status_interval = 0;
+String status_period = "";
+
 
 NTPtime NTPch("pool.ntp.org");   // Choose server pool as required
 
@@ -100,7 +95,10 @@ JsonObject getJSonFromFile(DynamicJsonDocument *doc, String filename, bool force
 
 int get_distance() {
   int calculated_distance = 0;
-  // Clears the trigPin condition
+  int avarage = 0;
+  for (size_t i = 0; i < 30; i++)
+  {
+     // Clears the trigPin condition
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
@@ -111,6 +109,9 @@ int get_distance() {
   duration = pulseIn(echoPin, HIGH);
   // Calculating the distance
   calculated_distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  avarage = avarage + calculated_distance;
+  }
+  calculated_distance = avarage/30;
   return calculated_distance;
 }
 
@@ -143,9 +144,12 @@ String cesspoolStatus(){
   String currTime = checkTime();
   String status = "Yhteys laitteeseen saatu: " + currTime + ".\n";
   status += "\n";
-  status += "Kaivon syvyys: " + String(tempDist) + "\n";
-  status += "Yhdistetty nettiin\n";
-  status += "Vielä kai jotain\n";
+  status += "Kaivon syvyys: " + String(tempDist) + "cm.\n";
+  status += "warn_level: " + String(warn_level) + "cm.\n";
+  status += "status_period: " +  status_period + ".\n";
+  status += "status_interval: " +  String(status_interval) + "cm.\n";
+  status += "top_distance: " +  String(top_distance) + "cm.\n";
+  status += "diameter: " +  String(diameter) + ".\n";
 
   return status;
 }
@@ -167,43 +171,208 @@ void handleNewMessages(int numNewMessages)
     if (from_name == "")
       from_name = "Guest";
 
-    if (text == "/mittaus")
-    {
+    if (text == "/mittaus"){
       distance = get_distance();
       
       bot.sendMessage(chat_id, "Pinnan etäisyys kannesta: " + String(distance) + "cm", "");
     }
 
-    if (text == "/ledoff")
-    {
-      ledStatus = 0;
-      digitalWrite(led, LOW); // turn the LED off (LOW is the voltage level)
-      bot.sendMessage(chat_id, "Led is OFF", "");
+    if (text == "/settings"){
+     String settings = "Settings -valikko, ei toimi, ellei joku tätä päivitä joskus. SD kortilla olevan tiedoston avulla voi hoitaa päivityksen:\n";
+      settings += "Alla olevilla komennoilla voit antaa käskyjä botille.\n\n";
+      settings += "/warn_level : Voit vaihtaa hälytyskorkeuden lähettämällä numeron senttimetreinä.\n";
+      settings += "/automation_settings : Automaattisen mittauksen asetus.\n";
+      settings += "/ssid : Syötä Wifin nimi.\n";
+      settings += "/password : Syötä Wifin salasana.\n";
+      settings += "/cancel : Peruuta valinnat\n";
+      settings += "/save : Tallenna valinnat\n";
+      
+      bot.sendMessage(chat_id, settings, "Markdown");
+      settings_menu = true;
+      unsigned long settingsTime = millis();
     }
 
-    if (text == "/status")
-    { 
+    if (text == "/restart"){
+      ESP.restart();
+    }
+
+    if (text == "/status"){ 
       bot.sendMessage(chat_id, cesspoolStatus(), ""); 
     }
 
-    if (text == "/start")
-    {
+    if (text == "/start"){
       String welcome = "Hei, tässä ohjeet septitankkibottia varten, " + from_name + ".\n";
       welcome += "Alla olevilla komennoilla voit antaa käskyjä botille.\n\n";
       welcome += "/status : kertoo tietoja laitteesta\n";
-      welcome += "/mittaus : Mittaa pinnan etäisyyden kanteen\n";
-      welcome += "/reset : Antaa resetointikäskyn\n";
+      welcome += "/mittaus : Mittaa pinnan etäisyyden\n";
+      welcome += "/restart : Antaa resetointikäskyn\n";
+      
       bot.sendMessage(chat_id, welcome, "Markdown");
     }
   }
 }
 
-void timedMeasure(String every, int interval){
-
-
-  //todo
-  
+void readVariables(){
+  BOT_TOKEN = String(configJson["bot_token"]);
+  MAIN_CHAT_ID = String(configJson["main_chat_id"]);
+  ssid = String(configJson["ssid"]);
+  password = String(configJson["password"]);
+  warn_level = configJson["warn_level"];
+  top_distance = configJson["top_distance"];
+  diameter = configJson["diameter"];
+  status_interval = configJson["status_interval"];
+  status_period = String(configJson["status_period"]);
 }
+
+void timedMeasure(){
+  checkTime();
+  int every_switch = 0;
+  int counter = 0;
+  if (status_period.equals("Y")){every_switch = 1;}
+  if (status_period.equals("M")){every_switch = 2;}
+  if (status_period.equals("D")){every_switch = 3;}
+  if (status_period.equals("H")){every_switch = 4;}
+  if (status_period.equals("MIN")){every_switch = 5;}
+  if (status_period.equals("S")){every_switch = 6;}
+  
+  switch (every_switch)
+  {    
+    case 1:
+      if (last_time_measured != dateTime.year){
+        if (dateTime.year >=  status_interval +last_time_measured){ 
+          if (dateTime.hour == 12){
+            get_distance();
+            Serial.println("Timed measure taken. Measured distance: " + String(distance) + "cm");
+            bot.sendMessage(MAIN_CHAT_ID, "Ajastettu mittaus. Pinnan etäisyys: "
+                                        + String(distance) + "cm. Tarkastus tehdään " +String(status_interval) +" vuoden välein", "");
+            last_time_measured =  dateTime.year;
+          }
+        }
+      }
+      break;
+    case 2:
+      if (last_time_measured != dateTime.month){
+        if (status_interval +last_time_measured >12){counter = (status_interval +last_time_measured) % 12;}
+        else{ counter = status_interval +last_time_measured;}
+        
+        if (dateTime.month >=  counter){
+          if (dateTime.hour == 12){
+          get_distance();
+          Serial.println("Timed measure taken. Measured distance: " + String(distance) + "cm");
+          bot.sendMessage(MAIN_CHAT_ID, "Ajastettu mittaus. Pinnan etäisyys: "
+                                        + String(distance) + "cm. Tarkastus tehdään " +String(status_interval) +" kuukauden välein.", "");
+          last_time_measured =  dateTime.month;
+          }
+        }
+      }
+      break;
+    case 3:
+        if (last_time_measured != dateTime.day){
+          if (status_interval +last_time_measured >28){counter = (status_interval +last_time_measured) % 28;}
+          else{ counter = status_interval +last_time_measured;}
+          
+          if (dateTime.day >=  counter){
+            if (dateTime.hour == 12){
+            get_distance();
+            Serial.println("Timed measure taken. Measured distance: " + String(distance) + "cm");
+            bot.sendMessage(MAIN_CHAT_ID, "Ajastettu mittaus. Pinnan etäisyys: "
+                                          + String(distance) + "cm. Tarkastus tehdään " +String(status_interval) +" päivän välein.", "");
+            last_time_measured =  dateTime.day;
+          }
+        }
+      }
+      break;
+  case 4:
+    if (last_time_measured != dateTime.hour){
+      if (status_interval +last_time_measured >24){counter = (status_interval +last_time_measured) % 24;}
+      else{ counter = status_interval +last_time_measured;}
+
+      if (dateTime.hour >= counter){   
+        get_distance();
+        Serial.println("Timed measure taken. Measured distance: " + String(distance) + "cm");
+        bot.sendMessage(MAIN_CHAT_ID, "Ajastettu mittaus. Pinnan etäisyys: "
+                                      + String(distance) + "cm. Tarkastus tehdään " +String(status_interval) +" tunnin välein.", "");
+        last_time_measured =  dateTime.hour;     
+      }
+    }  
+      break;
+  case 5:
+    if (last_time_measured != dateTime.minute){
+      if (status_interval +last_time_measured >60){counter = (status_interval +last_time_measured) % 60;}
+      else{ counter = status_interval +last_time_measured;}
+
+      if (dateTime.minute >=  counter){   
+        get_distance();
+        Serial.println("Timed measure taken. Measured distance: " + String(distance) + "cm");
+        bot.sendMessage(MAIN_CHAT_ID, "Ajastettu mittaus. Pinnan etäisyys: "
+                                      + String(distance) + "cm. Tarkastus tehdään " +String(status_interval) +" minuutin välein.", "");
+        last_time_measured =  dateTime.minute;     
+      }
+    }     
+      break;
+  case 6:
+    if (last_time_measured != dateTime.second){
+      if (status_interval +last_time_measured >50){counter = (status_interval +last_time_measured) % 60;}
+      else{ counter = status_interval +last_time_measured;}
+      Serial.println("Counter: " + String(counter) +", Second: " + String(dateTime.second) + ", Status interval: " + String(status_interval));
+      
+      if (dateTime.second >=  counter){   
+        get_distance();
+        Serial.println("Timed measure taken. Measured distance: " + String(distance) + "cm");
+        bot.sendMessage(MAIN_CHAT_ID, "Ajastettu mittaus. Pinnan etäisyys: "
+                                      + String(distance) + "cm. Tarkastus tehdään " +String(status_interval) +" sekunnin välein.", "");
+        last_time_measured =  dateTime.second;     
+      }
+    }
+      break;
+    default:
+      break;
+  }
+}
+
+bool saveJSonToAFile(DynamicJsonDocument *doc, String user_conf_file) {
+    SD.remove(user_conf_file);
+ 
+    // open the file. note that only one file can be open at a time,
+    // so you have to close this one before opening another.
+    Serial.println(F("Open file in write mode"));
+    configFileSDcard = SD.open(user_conf_file, FILE_WRITE);
+    if (configFileSDcard) {
+        Serial.print(F("Filename --> "));
+        Serial.println(user_conf_file);
+ 
+        Serial.print(F("Start write..."));
+ 
+        serializeJson(*doc, configFileSDcard);
+ 
+        Serial.print(F("..."));
+        // close the file:
+        configFileSDcard.close();
+        Serial.println(F("done."));
+ 
+        return true;
+    } else {
+        // if the file didn't open, print an error:
+        Serial.print(F("Error opening "));
+        Serial.println(user_conf_file);
+ 
+        return false;
+    }
+}
+
+void check_warnlevel(){
+      if (distance < configJson["warn_level"]){
+        if( abs(warning1 -distance) >= 3 && distance > 2){
+          bot.sendMessage(MAIN_CHAT_ID, "Pinnan etäisyys: " + String(distance) + "cm, on aika tilata tyhjennys", "");
+          warning1 = distance;
+        }
+        digitalWrite(led, HIGH);
+      }
+
+      else{
+        digitalWrite(led, LOW);
+      }
+    }
 
 void setup()
 {
@@ -223,19 +392,21 @@ void setup()
 
   //haetaan data jsonista
   configJson = getJSonFromFile(&config_doc, user_conf_file);
-  if (!configJson.isNull()){
+  if (configJson.isNull()){
     configJson = getJSonFromFile(&config_doc, conf_file);
     Serial.println("Using default conf file.");
   }
-
+  else
+  {
+    Serial.println("Found user Conf file.");
+  }
+  
+  readVariables();
+  
   configTime(2, 1, "pool.ntp.org");
 
   Serial.println("##  Connecting to Internet  ##");
   WiFi.mode(WIFI_STA);
-  ssid = String(configJson["ssid"]);
-  password = String(configJson["password"]);
-  BOT_TOKEN = String(configJson["bot_token"]);
-  MAIN_CHAT_ID = String(configJson["main_chat_id"]);
 
   bot.updateToken(BOT_TOKEN);
 
@@ -258,42 +429,56 @@ void setup()
   bot.sendMessage(MAIN_CHAT_ID, cesspoolStatus(), "");
 }
 
-void loop()
-{
+void loop(){
+  while (WiFi.status() == WL_CONNECTED){
 
-if (millis() - bot_lasttime > BOT_MTBS)
-  {
-    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    if (millis() - bot_lasttime > BOT_MTBS){
+      int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
 
-    while (numNewMessages)
-    {
-      Serial.println("got response");
-      handleNewMessages(numNewMessages);
-      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+      while (numNewMessages){
+        Serial.println("got response");
+        handleNewMessages(numNewMessages);
+        numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+      }
+
+      bot_lasttime = millis();
     }
-
-    bot_lasttime = millis();
-  }
   
-  distance = get_distance();
-  Serial.println("Measured distance: " + String(distance) + "cm");
+    distance = get_distance();
+    Serial.println("Continous measuring. Distance: " + String(distance) + "cm");
   
+    timedMeasure();
 
-  if (distance < configJson["warn_level"]){
+    check_warnlevel();
+
+    delay(200);
+
+    // while (settings_menu){
+    //   if( millis(); >= settingsTime + 100000){
+    //     settings_menu = false
+    //     Serial.println("Settings menu timed out");
+    //   }
+
+    // }
     
-    if( abs(warning1 -distance) >= 3 && distance > 2){
-     
-      bot.sendMessage(MAIN_CHAT_ID, "Pinnan etäisyys kannesta: " + String(distance) + "cm, on aika tilata tyhjennys", "");
-      warning1 = distance;
-    }
-    digitalWrite(led, HIGH);
-  }
-
-  else
-  {
-    digitalWrite(led, LOW);
-  }
-
-  delay(1500);
   
+  }
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    digitalWrite(led, HIGH);
+    Serial.print(".");
+    
+  }
+
+
 }
+/*
+/
+/
+/
+/
+/
+/
+/
+*/
